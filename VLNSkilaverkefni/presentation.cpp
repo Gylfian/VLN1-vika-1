15c5483bad15331ceda56794a9342662add69ae6
Gylfian <gylfia13@ru.is>
2015-11-29 14:24:04 +0000

    Presentation layer+

--- VLNSkilaverkefni/presentation.cpp
+++ VLNSkilaverkefni/presentation.cpp
@@ -1,30 +1,35 @@
 #include "main.h"
 #include "presentation.h"
 #include <stdlib.h>
+#include <conio.h>
 
 Presentation::Presentation() {}
 
 void Presentation::mainPage()
 {
-    cout << "What do you want to do?" << endl;
-    cout << "1) Add computer scientists to the list" << endl;
-    cout << "2) See the list of computer scientists" << endl;
-    cout << "Press any other key to quit" << endl;
+    cout << " ________________________________________" << endl;
+    cout << "|---------What do you want to do?--------|" << endl;
+    cout << "| 1) Add computer scientists to the list |" << endl;
+    cout << "| 2) See the list of computer scientists |" << endl;
+    cout << "| ------Press any other key to quit----- |" << endl;
+    cout << "|________________________________________|" << endl;
     options();
 }
 
 void Presentation::options()
 {
-    char num;
-    cin >> num;
+    char ans = getch();
+    system("CLS");
 
-    switch (num)
+    switch (ans)
     {
         case ('1'):
             cout << "Flott hja ther" << endl;
             break;
         case ('2'):
-            cout << "Dugleg/ur" << endl;
+            {
+                displayListOptions();
+            }
             break;
         default:
             exit (1);
@@ -33,11 +38,11 @@ void Presentation::options()
 
 bool Presentation::another()
 {
-    char answer;
-    cout << "Do you wish to add another person (y/n)" << endl;
-    cin >> answer;
 
-    switch (answer)
+    cout << "Do you wish to add another person (y/n) ";
+    char ans = getch();
+
+    switch (ans)
     {
         case ('Y'):
         case ('y'):
@@ -50,4 +55,56 @@ bool Presentation::another()
         default:
             return false;
     }
+}
+
+void Presentation::displayListOptions()
+{
+    cout << "How do you want the list displayed? " << endl;
+    cout << "1) Alphabetically" << endl;
+    cout << "2) By gender" << endl;
+    cout << "3) By year of birth" << endl;
+    cout << "4) By year of death" << endl;
+    cout << "Press any other key to go back" << endl;
+    listOptions();
+
+}
+
+void Presentation::listOptions()
+{
+    char ans = getch();
+    system("CLS");
+
+    switch(ans)
+    {
+        case('1'):
+        {
+
+        }
+        break;
+        case ('2'):
+        {
+
+        }
+        break;
+        case ('3'):
+        {
+
+        }
+        break;
+        case ('4'):
+        {
+
+        }
+        default:
+            mainPage();
+
+    }
+}
+
+bool Presentation::ascendingDecending()
+{
+    cout << "In what alphabetical order do you want the list?" << endl;
+    cout << "1) Ascending" << endl;
+    cout << "2) Decending" << endl;
+
 }
--- VLNSkilaverkefni/presentation.h
+++ VLNSkilaverkefni/presentation.h
@@ -8,8 +8,13 @@ class Presentation
 public:
     Presentation();
     void mainPage();
+    void getAns();
     void options();
     bool another();
+    void listOptions();
+    void displayListOptions();
+    bool ascendingDecending();
+
 };
 
 #endif // PRESENTATION_H
