8a471f51ff74c128819b5d16be901a05ba7ddb89
Gylfian <gylfia13@ru.is>
2015-12-03 10:34:56 +0000

    Sma fix

--- VLNSkilaverkefni/computerscientist.cpp
+++ VLNSkilaverkefni/computerscientist.cpp
@@ -18,34 +18,42 @@ CScientist::CScientist(string cName, string cGender, string date1, string date2)
 }
 
 
-string CScientist::getName() const{
+string CScientist::getName() const
+{
     return name;
 }
 
-string CScientist::getGender() const{
+string CScientist::getGender() const
+{
    return gender;
 }
 
-string CScientist::getDob() const{
+string CScientist::getDob() const
+{
    return dateBorn;
 }
 
-string CScientist::getDod() const{
+string CScientist::getDod() const
+{
    return dateDeath;
 }
 
-void CScientist::setName(string nameToSet){
+void CScientist::setName(string nameToSet)
+{
     name=nameToSet;
 }
 
-void CScientist::setGender(string genderToSet){
+void CScientist::setGender(string genderToSet)
+{
     gender=genderToSet;
 }
 
-void CScientist::setDob(string dateToSet){
+void CScientist::setDob(string dateToSet)
+{
     dateBorn=dateToSet;
 }
 
-void CScientist::SetDod(string dateToSet){
+void CScientist::SetDod(string dateToSet)
+{
     dateDeath=dateToSet;
 }
